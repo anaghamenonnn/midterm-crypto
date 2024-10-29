ae6e9a72af4e42d04d158ab67c0df3127383f59f
anaghamenonnn <112660061+anaghamenonnn@users.noreply.github.com>
2024-10-29 08:06:53 -0400

    commit message

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Foot.css'
+
+const footer = () => {
+  return (
+    <div>
+
+        <p> Copyright @ 2024, Cryptoplace - All Right Reserved.</p>
+      
+    </div>
+  )
+}
+
+export default footer
